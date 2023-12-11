b33559084a876d532e3cbfd4d8b73553ba7f3e82
Ssamk-kuu-ruu <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2023-12-11 14:35:39 +0800

    Update final.cs

--- final.cs
+++ final.cs
@@ -37,4 +37,77 @@ class ASEANStudent
   }
 
   public int CountryCode
+  {
+    get;
+    set;
+  }
+
+  public int AreaCode
+  {
+    get;
+    set;
+  }
+
+  public int PhoneNumber
+  {
+    get;
+    set;
+  }
+}
+
+class ASEANPhonebook
+{
+  private static List<ASEANStudent> phonebook = new List<ASEANStudent>;
+
+  static void Main (string[] args)
+  {
+    while (true)
+    {
+      DisplayMainMenu();
+      MenuChoice choice = GetmenuChoice();
+
+      switch(choice)
+      {
+        case MenuChoice.Store:
+          Store();
+          break;
+
+        case MenuChoice.Edit:
+          Edit();
+          break;
+
+        case MenuChoice.Search;
+          Search();
+          break;
+
+        case menuChoice.Exit;
+          Console.WriteLine("Thank you for using the ASEAN Phonebook!");
+          return;
+      }
+    }
+  }
+
+  static voic DisplayMainMenu()
+  {
+    Console.WriteLine("1. Store to ASEAN Phonebook");
+    Console.WriteLine("2. Edit entry to ASEAN Phonebook");
+    Console.WriteLine("3. Search ASEAN phonebook by country");
+    Console.WriteLine("4. Exit");
+  }
+
+  static MenuChoice GetMenuChoice()
+  {
+    Console.Write("Enter your choice: ");
+    string input = Console.Readline();
+
+    if (TryParseInt(input, out int choice) && Enum.IsDefined(typeof(MenuChoice), choice))
+    {
+      return (MenuChoice)choice;
+    }
+    Console.WriteLine("Invalid choice. Please try again.");
+    return int.TryParse(input, out result);
+  }
+
+  // Giana galaw galaw... pagod na ko xD
+  // To becontinued by Giana hehe
 }
