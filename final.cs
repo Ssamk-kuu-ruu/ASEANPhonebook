91907c20b2f4658be10bdc5214a0474f45eaf97d
Ssamk-kuu-ruu <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2023-12-11 13:52:48 +0800

    Update final.cs

--- final.cs
+++ final.cs
@@ -4,3 +4,37 @@ using System.Linq;
 
 enum MenuChoice
 {
+  Store = 1,
+  Edit,
+  Search,
+  Exit
+}
+
+class ASEANStudent
+{
+  public int StudentNumber
+  {
+    get;
+    set;
+  }
+
+  public string Surname
+  {
+    get;
+    set;
+  }
+
+  public string FirstName
+  {
+    get;
+    set;
+  }
+
+  public string Occupation
+  {
+    get;
+    set;
+  }
+
+  public int CountryCode
+}
